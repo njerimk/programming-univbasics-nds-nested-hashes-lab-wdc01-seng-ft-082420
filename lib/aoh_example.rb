795556c90b85dbca5be80896142e85af2e5f2a84
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-13 14:48:40 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,9 +31,8 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 aoh = [{:name => "Don Gately", :occupation =>"Live-in Staff"}, {:name =>"Joelle van Dyne", :occupation =>"Radio Personality"}, {:name => "Pat Monteseian", :occupation => "Staff"}, {:name => "Kate Gompert", :occupation =>"None"}, {:name =>"Bruce Green", :occupation => "Fan of Mildred"}]
+aoh[row][key]= new_value
 
-puts aoh[row][key]= new_value
-puts aoh
 end
 
 aoh = [{:name => "Don Gately", :occupation =>"Live-in Staff"}, {:name =>"Joelle van Dyne", :occupation =>"Radio Personality"}, {:name => "Pat Monteseian", :occupation => "Staff"}, {:name => "Kate Gompert", :occupation =>"None"}, {:name =>"Bruce Green", :occupation => "Fan of Mildred"}]
