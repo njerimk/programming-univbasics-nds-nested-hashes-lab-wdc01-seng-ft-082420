c3cda1fc6a42f3e5461c430e48a84d27928be5a4
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-13 13:57:14 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,6 +23,8 @@ inifinite_jest = [{:name => "Don Gately", :occupation =>"Live-in Staff"}, {:name
 end
 
 def aoh_lookup(aoh, row, key)
+aoh = [{:name => "Don Gately", :occupation =>"Live-in Staff"}, {:name =>"Joelle van Dyne", :occupation =>"Radio Personality"}, {:name => "Pat Monteseian", :occupation => "Staff"}, {:name => "Kate Gompert", :occupation =>"None"}, {:name =>"Bruce Green", :occupation => "Fan of Mildred"}]
+aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
