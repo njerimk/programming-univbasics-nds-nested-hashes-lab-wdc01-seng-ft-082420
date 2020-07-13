dddc101b966c83fb6261892d752a3d880601cd83
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-13 13:58:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -32,5 +32,6 @@ def aoh_update(aoh, row, key, new_value)
   # Return the updated AoH
 aoh = [{:name => "Don Gately", :occupation =>"Live-in Staff"}, {:name =>"Joelle van Dyne", :occupation =>"Radio Personality"}, {:name => "Pat Monteseian", :occupation => "Staff"}, {:name => "Kate Gompert", :occupation =>"None"}, {:name =>"Bruce Green", :occupation => "Fan of Mildred"}]
 
-aoh[row][key]= new_value  
+new_aoh= aoh[row][key]= new_value
+
 end
