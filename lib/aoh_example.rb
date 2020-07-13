ed4ad0acab93c00d564e9a5207a93bde9cf42b92
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-13 13:50:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,6 +11,8 @@ def assembled_aoh
   # Array. Ruby constants are denoted by ALL_CAPS
   inifinite_jest = [{:name => "Don Gately", :occupation =>"Live-in Staff"}, {:name =>"Joelle van Dyne", :occupation =>"Radio Personality"}, {:name => "Pat Monteseian", :occupation => "Staff"}, {:name => "Kate Gompert", :occupation =>"None"}, {:name =>"Bruce Green", :occupation => "Fan of Mildred"}]
   
+  puts inifinite_jest
+  
 end
 
 inifinite_jest = [{:name => "Don Gately", :occupation =>"Live-in Staff"}, {:name =>"Joelle van Dyne", :occupation =>"Radio Personality"}, {:name => "Pat Monteseian", :occupation => "Staff"}, {:name => "Kate Gompert", :occupation =>"None"}, {:name =>"Bruce Green", :occupation => "Fan of Mildred"}]
